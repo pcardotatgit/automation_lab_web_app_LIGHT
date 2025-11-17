1d2fbd2c3d06e5609c1f4c1f54a31cd1e43bd1e1
Patrick Cardot <patrick.cardot@gmail.com>
2025-11-17 09:03:03 +0100

    Update z_init_appli.py

--- z_init_appli.py
+++ z_init_appli.py
@@ -1,5 +1,5 @@
 '''
-    modification : 20251111
+    modification : 20251115
     
     description : initialize the application, create empty files  clean folder
 '''
@@ -11,44 +11,11 @@ ok_delete=1
 
 
 def init_appli():
+    with open('./venv/Scripts/activate.bat') as file:
+        text_content=file.read()
+    text_content=text_content.replace(':END','python app.py\n:END')
     with open('./venv/Scripts/activate.bat','w') as file:
-        line_out='''@echo off
-
-rem This file is UTF-8 encoded, so we need to update the current code page while executing it
-for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
-    set _OLD_CODEPAGE=%%a
-)
-if defined _OLD_CODEPAGE (
-    "%SystemRoot%\System32\chcp.com" 65001 > nul
-)
-
-set VIRTUAL_ENV=C:\patrick\Python_DEV\current_dev\python\z_poc_simplification\venv
-
-if not defined PROMPT set PROMPT=$P$G
-
-if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
-if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
-
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(venv) %PROMPT%
-
-if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
-set PYTHONHOME=
-
-if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
-if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
-
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venv) 
-python app.py
-:END
-if defined _OLD_CODEPAGE (
-    "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
-    set _OLD_CODEPAGE=
-)
-
-        '''
-        file.write(line_out)    
+        file.write(text_content)    
     os.remove("a.bat")
     os.remove("b.bat")
     os.remove("c.bat")
@@ -57,11 +24,11 @@ if defined _OLD_CODEPAGE (
     with open('a.bat','w') as file:
         file.write('venv\\scripts\\activate')    
     with open('b.bat','w') as file:
-        file.write('python compile.py')        
+        file.write('python app.py')        
     with open('port.txt','w') as file:
         file.write('4000')     
     with open('server_ip_address.txt','w') as file:
-        file.write('localhost')          
+        file.write('localhost')           
                 
 if __name__=="__main__":
     init_appli()    
